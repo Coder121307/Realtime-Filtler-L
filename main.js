ab3ee635b8788ad635f6083b45d54c614c205bda
Coder121307 <78479019+Coder121307@users.noreply.github.com>
2021-09-25 09:38:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+}
+
+function canvas(){
+    canvas = createCanvas(1000,1000);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_Snapshot(){
+    save('MyFilterImage.png');
+}
+
